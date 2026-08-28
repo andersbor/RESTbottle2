89ea3cb2b7e8897ee3f7d8d07bd377fd9ed154e7
andersbor <anbo@zealand.dk>
2026-08-28 20:20:20 +0200

    Add project files.

---
+++ RESTbottle2/Models/Bottle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RESTbottle2.Models
+{
+    public class Bottle
+    {
+        public int Id { get; set; }
+        public double Volume { get; set; }
+        public string? Name { get; set; } 
+
+        
+
+        public override string ToString()
+        {
+            return $"Bottle: {Name}, Volume: {Volume}";
+        }
+    }
+}
---
+++ RESTbottle2/Models/BottlesRepositry.cs
@@ -0,0 +1,43 @@
+﻿namespace RESTbottle2.Models
+{
+    public class BottlesRepositry
+    {
+        private List<Bottle> _bottles = new List<Bottle>();
+        private int nextId = 1;
+
+        public List<Bottle> Get() {
+            return _bottles.ToList();  
+        }
+
+        public Bottle AddBottle(Bottle b) { 
+            b.Id = nextId++;
+            _bottles.Add(b);
+            return b;
+        }
+
+        public Bottle? GetById(int id)
+        {
+            return _bottles.FirstOrDefault(b => b.Id == id);
+        }
+
+        public Bottle? DeleteById(int id) {
+            Bottle? bottle = GetById(id);
+            if (bottle != null) {
+                _bottles.Remove(bottle);
+            }
+            return bottle;
+        }
+
+        public Bottle? Update(int id, Bottle data)
+        {
+            Bottle? bottle = GetById(id);
+            if (bottle != null)
+            {
+                bottle.Volume = data.Volume;
+                bottle.Name = data.Name;
+            }
+            return bottle;
+        }
+
+    }
+}
